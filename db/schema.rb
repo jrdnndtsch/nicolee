6de8c56d9a6648d7d50223cb78744e95fda60c64
Jordan Deutsch <jordanbdeutsch@gmail.com>
2015-09-23 09:34:49 -0400

    added authentication to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923131730) do
+ActiveRecord::Schema.define(version: 20150923131921) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -34,7 +34,10 @@ ActiveRecord::Schema.define(version: 20150923131730) do
     t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "translations", ["user_id"], name: "index_translations_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
